c4e702ae291d6d0e8f757b0adac0a24d415e2301
Erm010v <Erm010v@yandex.ru>
2023-08-15 17:24:19 +0300

    Lec_1_API_JAVA
    Lec_1_API_JAVA

--- Lecture_1_prog.java
+++ Lecture_1_prog.java
@@ -1,3 +1,82 @@
-// public class program {
-// // static public void main(String[] args)
-// }
+public class Lecture_1_prog {
+    public static void main(String[] args) {
+        // // // System.out.println("Goodbye world");
+        // // String s = null; // необходимо положить значение в переменную
+        // // System.out.println(s);
+        // // }
+        // // }
+
+        // // Основы: типы данных
+        // // public class Program {
+        // public static void main(String[] args) {
+        // // // примитивные типы данных
+        // // short age = 10;
+        // // int salary = 123456;
+        // // System.out.println(age); // 10
+        // // System.out.println(salary); // 123456
+        // // // вещественные числа, суф f необходим для float
+        // // float e = 2.7f;
+        // // // double f = 123.450;
+        // // double pi = 3.1415;
+        // // System.out.println(e); // 2.7
+        // // System.out.println(pi); // 3.1415
+        // // // тип данных char
+        // // char ch = '1';
+        // // System.out.println(Character.isDigit(ch)); // true
+        // // ch = 'a';
+        // // System.out.println(Character.isDigit(ch));
+        // // // тип данных boolean
+        // // boolean flag1 = 123 <= 234;
+        // // System.out.println(flag1); // true
+        // // boolean flag2 = 123 >= 234 || flag1;
+        // // System.out.println(flag2); // true
+        // // boolean flag3 = flag1 ^ flag2; // разделительная дизъюнкция True выдает
+        // False
+        // // System.out.println(flag3); // false
+        // // // строки не простые, сразу не известно, сколько в ней хранится
+        // // // 1 символ = 2 байтам, умножаем кол-во символов на 2
+        // // String msg = "Hello world";
+        // // System.out.println(msg); // Hello world
+        // // // Неявная типизация
+        // // var a = 123;
+        // // System.out.println(a); // 123
+        // // var d = 123.456;
+        // // System.out.println(d); // 123.456
+        // // Неявная типизация
+        // // var a = 123;
+        // // System.out.println(a); // 123
+        // // var d = 123.456;
+        // // System.out.println(d); // 123.456
+        // // System.out.println(getType(a)); // Integer
+        // // System.out.println(getType(d)); // Double
+        // // d = 1022;
+        // // System.out.println(d); // 1022
+        // // d = "mistake";
+        // // error: incompatible types:
+        // // String cannot be converted to double
+
+        // static String getType(Object o) {
+        // return o.getClass().getSimpleName();
+        // }
+
+        // Преобразования
+        int i = 123;
+        double d = i;
+        System.out.println(i); // 123
+        System.out.println(d); // 123.0
+        d = 3.1415;
+        i = (int) d;
+        System.out.println(d); // 3.1415
+        System.out.println(i); // 3
+        d = 3.9415;
+        i = (int) d;
+        System.out.println(d); // 3.9415
+        System.out.println(i); // 3
+        byte b = Byte.parseByte("123");
+        System.out.println(b); // 123
+        b = Byte.parseByte("1234");
+        System.out.println(b); // NumberFormatException: Value out of range
+
+    }
+
+}
--- program.java
+++ program.java
@@ -1,19 +1,95 @@
+import java.util.Scanner;
 
 public class program {
-    public static void main(String[] args) {
-        // System.out.println("Goodbye world");
-        String s = null; // необходимо положить значение в переменную
-        System.out.println(s);
-    }
-}
+    // public static void main(String[] args) {
+    // Scanner iScanner = new Scanner(System.in);
+    // System.out.printf("Name: ");
+    // int x = iScanner.nextInt();
+    // System.out.printf("double a: ");
+    // double y = iScanner.nextDouble();
+    // System.out.printf("Привет, %f = %f", x, y, x + y);
+    // iScanner.close();
+    // }
+    // }
 
-// Основы: типы данны
-public class Program {
+    // public class program {
+    // public static void main(String[] args) {
+    // int a = 123;
+    // a = a-- - --a; // 2
+    // a = --a - a--; // 0
+    // постфиксный и префиксный инкременты
+    // System.out.println(++a);
+    // System.out.println(a++);
+    // System.out.println(a);
+
+    // Побитовый сдвиг
+    // int a = 8;
+    // // 1000
+    // // a = a << 1;
+    // System.out.println(a << 1);
+    // 1000 - добавился 1 бит 0 = 10000
+
+    // int a = 16;
+    // // 10010
+    // System.out.println(a >> 1);
+    // // 1001 - получается
+    // int a = 5; // в двоичном представлении 101
+    // int b = 2; // в двоичном представлении 010
+    // System.out.println(a | b); // ответ 111 или 7
+    // System.out.println(a & b); // ответ 000 или 0
+    // System.out.println(a ^ b); // ответ 111 или 7
+    // String s = "qwer"; // 4 символа, индекс 4-ки это 3, т.к. индексы от 0..до 3 в
+    // qwer
+    // boolean b = s.length() > 5 && s.charAt(4) = '1'; // проверка условия
+    // System.out.println(a & b);
+    // System.out.println(a && b);
+    // System.out.println(b);
+
+    // Массивы
+
+    // Одномерные
+
+    // int[] arr = new int[10];
+    // System.out.println(arr.length); // 10
+    // arr = new int[] { 1, 2, 3, 4, 5 };
+    // System.out.println(arr.length); // 5
+
+    // Многомерные
+
+    // int[] arr[] = new int[3][5]; // массив массивов int[][]
+    // for (int[] line : arr) {
+    // for (int item : line) {
+    // System.out.printf("%d ", item);
+    // }
+    // System.out.println();
+    // }
+    // 2
+    // int[][] arr = new int[3][5];
+
+    // for (int i = 0; i < arr.length; i++) {
+    // for (int j = 0; j < arr[i].length; j++) {
+    // System.out.printf("%d ", arr[i][j]);
+    // }
+    // System.out.println();
+    // }
+
+    // Проверка на соответствие получаемого типа
+    // flag либо True либо False
     public static void main(String[] args) {
-        float e = 2.7f;
-        double f = 123.450;
-        double pi = 3.1415;
-        System.out.println(e); // 2.7
-        System.out.println(pi); // 3.1415
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("int a: ");
+        boolean flag = iScanner.hasNextInt();
+        System.out.println(flag);
+        int i = iScanner.nextInt();
+        System.out.println(i);
+        iScanner.close();
+
+        // Форматированный вывод
+
+        int a = 1, b = 2;
+        int c = a + b;
+        String res = a + " + " + b + " = " + c;
+        System.out.println(res);
     }
+
 }
